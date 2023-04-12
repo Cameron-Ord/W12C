dae875ad6ec452ff6768d48829bc2e8ee41623c6
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-12 14:35:19 -0600

    commented the code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+//selecting the element//
 let indentity = document.querySelector(`#section_main`);
-
+//injecting the inputs and buttons//
 indentity.insertAdjacentHTML(`beforeend`,`
 <article>
 <span>
@@ -13,7 +14,7 @@ indentity.insertAdjacentHTML(`beforeend`,`
 
 
 
-
+//the failure function, provides a message on invalid login//
 function PW_POST_FAILURE(){
     let output = document.querySelector(`#section_main`);
 
@@ -21,7 +22,7 @@ function PW_POST_FAILURE(){
 
 
 }
-
+//the success function, sets a cookie to response and takes you to the home page//
 function PW_POST_SUCCESS(response){
 
     Cookies.set(`token`, `${response[`data`][`token`]}`);
@@ -32,49 +33,50 @@ function PW_POST_SUCCESS(response){
 
 
 
-
+//the login function//
 function User_Login(){
-
+//selects the pass_input element as pw_input//
     let pw_input = document.querySelector(`#pass_input`);
-
+//sets the pw_input_value to 'value', which is grabbed from what is writen in the input box//
     let pw_input_value = pw_input[`value`];
-
+//the same process from above on a different element//
     let em_input = document.querySelector(`#email_input`);
 
     let em_input_value = em_input[`value`];
 
     axios({
-
+//the axios command, using post//
         url:`https://reqres.in/api/login`,
 
         method: `POST`,
 
         data:{
 
-
+//using the variables created before the axios command//
             email: em_input_value,
             password: pw_input_value
 
 
         }
 
-
+//calling the functions based on the data given//
     }).then(PW_POST_SUCCESS).catch(PW_POST_FAILURE);
 
 
 }
 
+//selecting the login element and giving it a variable//
 let pw_button = document.querySelector(`#login`);
-
+//the event listener, calling the function on click//
 pw_button.addEventListener(`click`, User_Login);
 
-
+//getting the cookies and setting it as a variable//
 let Login_cookie = Cookies.get(`token`);
 
-
+//if the cookie is not undefined. provide a logout button//
 if(Login_cookie !== undefined){
 
-
+//injecting the respective html//
 indentity.insertAdjacentHTML(`beforeend`, `<button id="logout_button">logout</button>`);
     
 }
@@ -84,13 +86,13 @@ indentity.insertAdjacentHTML(`beforeend`, `<button id="logout_button">logout</bu
 
 
 function remove_cookie(){
-
+//using the cookies remove command to remove the selected cookie//
     Cookies.remove(`token`);
     
     
     }
     
     let logout_index = document.querySelector(`#logout_button`);
-    
+    //calling the function with a click//
     logout_index.addEventListener(`click`, remove_cookie);
     
--- js/home.js
+++ js/home.js
@@ -1,22 +1,26 @@
 
 let welcome = document.querySelector(`#welcome`);
-
+//selecting the element//
 let Get_Cookie = Cookies.get(`token`);
-
+//grabbing the cookies set by app.js//
 function GETsuccess(response){
 
+    //selecting the element inside the function//
     let axiosgetpage = document.querySelector(`#colors`)
 
+    //a loop that goes through first the data object, then the data array//
     for(let counter = 0; counter < response[`data`][`data`].length; counter = counter +1){
 
 
 
+        //injecting the HTML into #colors, and looping through the array inside the API//
         axiosgetpage.insertAdjacentHTML(`beforeend`,
         
         `
         <span id="color_span">
 
 
+        
         <div style="width: 200px; height:200px;background-color:${response[`data`][`data`][counter][`color`]};">
         <h2>${response[`data`][`data`][counter][`id`]}</h2>
 
@@ -40,7 +44,7 @@ function GETsuccess(response){
 
 
 function GETfailure(){
-
+//setting the failure function//
     let axiosgetpage = document.querySelector(`#welcome`);
     axiosgetpage.insertAdjacentHTML(`beforeend`, `<h3>Fatal request 400</h3>`);
 
@@ -53,10 +57,13 @@ function GETfailure(){
 if(Get_Cookie === undefined){
 
 
+    // if the cookie is not defined, print a failure message and provide a link to the login page//
     let axiosgetpage = document.querySelector(`#welcome`);
     axiosgetpage.insertAdjacentHTML(`beforeend`, `<h3>Request Failed <a href="/index.html">Please log in</a></h3>`);
     
 }else{
+
+    //otherwise, print a welcome and run the axios command, providing the data for GETsuccess and GETfailure//
 
 
     welcome.insertAdjacentHTML(`beforeend`, `<span>
@@ -69,7 +76,7 @@ if(Get_Cookie === undefined){
     
     `);
 
-    
+    //a logout button is provided//
     axios({
 
 
@@ -84,14 +91,16 @@ if(Get_Cookie === undefined){
     
 }
 
+//the function that the logout button uses//
 
 function remove_cookie(){
-
+//removes the login token cookie//
 Cookies.remove(`token`);
 
 
 }
 
+//the selector for the button//
 let logout_home = document.querySelector(`#logout_button`);
-
+//the event listener for the button, uses the function on click.//
 logout_home.addEventListener(`click`, remove_cookie)
